1eafe24998b9b5bdaa70fc7e69ebcdc3e1e32978
Giampiero Specogna <giampi_12@hotmail.com>
2017-03-24 18:20:42 -0400

    delayed animation

--- js/site.js
+++ js/site.js
@@ -2,16 +2,7 @@ function showEffects(){
 $("body").hide(0,showNav);
 }
 function showNav(){
-  $("#navDemo").fadeIn(5000,showContainer);
+  $("body").slideDown(10000);
 }
- function showContainer(){
-   $(".w3-container").slideDown(5000,showContent);
- }
- function showContent(){
-   $(".w3-content").slideDown(5000,showBody);
- }
- function showBody(){
-   $("body").slideDown(5000);
- }
 // Effects
 window.addEventListener("load",showEffects);
