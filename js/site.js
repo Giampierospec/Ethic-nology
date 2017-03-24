8afb7c9df2f1c20237552446628f6907bfdd9a2a
Giampiero Specogna <giampi_12@hotmail.com>
2017-03-24 17:59:38 -0400

    added some more effects

--- js/site.js
+++ js/site.js
@@ -1,10 +1,17 @@
 function showEffects(){
-$("body").hide(0,showBody);
+$("body").hide(0,showNav);
 }
-function showBody(){
-  $("body").fadeIn(5000);
+function showNav(){
+  $("#navDemo").fadeIn(5000,showContainer);
 }
-
-
+ function showContainer(){
+   $(".w3-container").slideDown(5000,showContent);
+ }
+ function showContent(){
+   $(".w3-content").slideDown(5000,showBody);
+ }
+ function showBody(){
+   $("body").slideDown(5000);
+ }
 // Effects
 window.addEventListener("load",showEffects);
